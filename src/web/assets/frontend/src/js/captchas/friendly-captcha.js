ac9ab04f39774ccf163476b4bba36eeb8085e7b4
Josh Crawford <joshua@sgroup.com.au>
2024-12-04 07:32:13 +1100

    Fix Captcha integrations not firing `validateCustom()` JS event

--- src/web/assets/frontend/src/js/captchas/friendly-captcha.js
+++ src/web/assets/frontend/src/js/captchas/friendly-captcha.js
@@ -130,7 +130,9 @@ export class FormieFriendlyCaptcha extends FormieCaptchaProvider {
         if (this.submitHandler) {
             // Run the next submit action for the form. TODO: make this better!
             if (this.submitHandler.validatePayment()) {
-                this.submitHandler.submitForm();
+                if (this.submitHandler.validateCustom()) {
+                    this.submitHandler.submitForm();
+                }
             }
         }
     }
